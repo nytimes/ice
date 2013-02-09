ff9aef4a3532a3db919b9771e2cfca0372d3615a
Johannes Wilm <johannes@wilm.no>
2013-02-09 16:54:32 -0300

    separate stub element code from contentEditable=false code. fix contenteditable

--- src/ice.js
+++ src/ice.js
@@ -933,14 +933,24 @@
                     range.collapse();
                     return false;
                 }
-
-                // If the next container is non-editable, look at the parent element instead. 
-                if (!nextContainer.isContentEditable) {
-                    nextContainer = nextContainer.parentElement;
+                
+                // If the next container is a text node, look at the parent node instead.
+                if (nextContainer.nodeType === ice.dom.TEXT_NODE) {
+                    nextContainer = nextContainer.parentNode;
+                }
+                
+                // If the next container is non-editable, enclose it with a delete ice node and add an empty text node after it to position the caret.
+                if (nextContainer.nodeType !== ice.dom.TEXT_NODE && !nextContainer.isContentEditable) {
+                    var returnValue = this._addNodeTracking(nextContainer, false, false);
+                    var emptySpaceNode = document.createTextNode('');
+                    nextContainer.parentNode.insertBefore(emptySpaceNode, nextContainer.nextSibling);
+                    range.selectNode(emptySpaceNode);
+                    range.collapse(true);
+                    return returnValue;
                 }
 
-                // If the caret was placed directly before a stub element or an element that is not editable, enclose the element with a delete ice node.
-                if (ice.dom.isChildOf(nextContainer, parentBlock) && ice.dom.isStubElement(nextContainer) || !nextContainer.isContentEditable) {
+                // If the caret was placed directly before a stub element, enclose the element with a delete ice node.
+                if (ice.dom.isChildOf(nextContainer, parentBlock) && ice.dom.isStubElement(nextContainer)) {
                     return this._addNodeTracking(nextContainer, range, false);
                 }
 
@@ -995,7 +1005,7 @@
                 initialContainer = range.startContainer,
                 initialOffset = range.startOffset,
                 commonAncestor = range.commonAncestorContainer;
-
+                
             // Handle cases of the caret is at the start of a container or outside a text node
             if (initialOffset === 0 || commonAncestor.nodeType !== ice.dom.TEXT_NODE) {
                 // If placed at the end of a container that cannot contain text, such as an ul element, place the caret at the end of the last item.
@@ -1033,18 +1043,28 @@
                 }
 
                 // Firefox finds an ice node wrapped around an image instead of the image itself some times, so we make sure to look at the image instead. 
-                if (ice.dom.is(prevContainer,  '.' + this._getIceNodeClass('insertType') + ', .' + this._getIceNodeClass('deleteType')) && prevContainer.childNodes.length > 0) {
-                    
+                if (ice.dom.is(prevContainer,  '.' + this._getIceNodeClass('insertType') + ', .' + this._getIceNodeClass('deleteType')) && prevContainer.childNodes.length > 0) {                  
                     prevContainer = prevContainer.lastChild;
                 }
-                // If the previous container is non-editable, look at the parent element instead. 
+                
+                // If the previous container is a text node, look at the parent node instead.
+                if (prevContainer.nodeType === ice.dom.TEXT_NODE) {
+                    prevContainer = prevContainer.parentNode;
+                }
+                    
+                // If the previous container is non-editable, enclose it with a delete ice node and add an empty text node before it to position the caret.
                 if (!prevContainer.isContentEditable) {
-                    prevContainer = prevContainer.parentElement;
+                    var returnValue = this._addNodeTracking(prevContainer, false, true);
+                    var emptySpaceNode = document.createTextNode('');
+                    prevContainer.parentNode.insertBefore(emptySpaceNode, prevContainer);
+                    range.selectNode(emptySpaceNode);
+                    range.collapse(true);
+                    return returnValue;
                 }
 
-                // If the caret was placed directly after a stub element or an element that is not editable, enclose the element with a delete ice node.
+                // If the caret was placed directly after a stub element, enclose the element with a delete ice node.
                 if (ice.dom.isStubElement(prevContainer) && ice.dom.isChildOf(prevContainer, parentBlock) || !prevContainer.isContentEditable) {
-                    return this._addNodeTracking(prevContainer, range, true);
+                     return this._addNodeTracking(prevContainer, range, true);
                 }
 
                 if (prevContainer !== parentBlock && !ice.dom.isChildOf(prevContainer, parentBlock)) {
