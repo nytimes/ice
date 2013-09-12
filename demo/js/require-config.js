fb2171f7952f46aaea7e24b1ab410676a0a606e2
Curt Mullin <catsgotmytongue@gmail.com>
2013-09-11 23:01:52 -0700

    work in progress on mce4 and requirejs

---
+++ demo/js/require-config.js
@@ -0,0 +1,66 @@
+var require = {
+    "paths": {
+      "jquery": "http://code.jquery.com/jquery-1.9.1.min",
+      'tinymce': "tinymce/js/tinymce/tinymce",
+      "tinymce.jquery": "tinymce/js/tinymce/jquery.tinymce.min",
+   //    "rangy": "rangy/rangy-core",
+   //    "ice": "ice/ice",
+   //    "icePluginManager": "ice/icePluginManager",
+   //    "IceAddTitlePlugin":'ice/plugins/IceAddTitlePlugin/IceAddTitlePlugin',
+	  // "IceCopyPastePlugin":'ice/plugins/IceCopyPastePlugin/IceCopyPastePlugin.js',
+	  // "IceEmdashPlugin":'ice/plugins/IceEmdashPlugin/IceEmdashPlugin.js',
+	  // "IceSmartQuotesPlugin":'ice/plugins/IceSmartQuotesPlugin/IceSmartQuotesPlugin.js',
+
+  "jquery1.9": "http://code.jquery.com/jquery-1.9.1.min",
+  "jquery-migrate" : "http://code.jquery.com/jquery-migrate-1.0.0",
+  "rangy-core": "../../lib/rangy/rangy-core",
+  "polyfills": "../../src/polyfills",
+  "ice":"../../src/ice",
+  "dom":"../../src/dom",
+  "icePlugin":"../../src/icePlugin",
+  "icePluginManager":"../../src/icePluginManager",
+  "bookmark":"../../src/bookmark",
+  "selection":"../../src/selection",
+  "IceAddTitlePlugin":"../../src/plugins/IceAddTitlePlugin/IceAddTitlePlugin",
+  "IceCopyPastePlugin":"../../src/plugins/IceCopyPastePlugin/IceCopyPastePlugin",
+  "IceEmdashPlugin":"../../src/plugins/IceEmdashPlugin/IceEmdashPlugin",
+  "IceSmartQuotesPlugin":"../../src/plugins/IceSmartQuotesPlugin/IceSmartQuotesPlugin",
+  "tinymce":"../../lib/tinymce/jscripts/tinymce4/tinymce",
+    },
+    "shim": {
+    	"jquery1.9": {
+    		exports: "$"
+    	},
+      "rangy-core": {
+        exports: "window.rangy"
+      },
+      "dom": {
+        deps:['ice'],
+        exports: "dom"
+      },
+      "selection" : {
+        exports: "Selection"
+      },
+      "bookmark": {
+        exports: "Bookmark"
+      },
+      "ice" : {
+        exports: "ice"
+      },
+      "icePluginManager": {deps: ['ice'] },
+      "IceAddTitlePlugin":{ deps: ['icePluginManager']},
+      "IceCopyPastePlugin":{ deps: ['icePluginManager']},
+      "IceEmdashPlugin":{ deps: ['icePluginManager']},
+      "IceSmartQuotesPlugin":{ deps: ['icePluginManager']},
+    	"tinymce": {
+    		exports: "tinymce",
+    		init: function() {
+    			this.tinymce.DOM.events.domLoaded = true;
+    			return this.tinymce;
+    		}
+    	},
+    	"tinymce.jquery" :{
+    		deps: ['jquery']
+    	}
+    }
+};
