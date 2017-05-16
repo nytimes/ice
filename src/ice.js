d438bde5c11fe91ee65899cd6d60beb1cf725686
Joey Rogues <jonathan.rogues@concordnow.com>
2017-05-16 17:26:29 +0200

    changeId is not a number, it's now a uuid

--- src/ice.js
+++ src/ice.js
@@ -107,7 +107,7 @@
 			_batchChangeid: null,
 
 			// Incremented for each new change, dropped in the changeIdAttribute.
-			_uniqueIDIndex: 1,
+			// _uniqueIDIndex: null,
 
 			// Temporary bookmark tags for deletes, when delete placeholding is active.
 			_delBookmark: 'tempdel',
@@ -866,7 +866,17 @@
 			},
 
 			getNewChangeId: function () {
-				var id = ++this._uniqueIDIndex;
+				var getUuid = function() {
+			    var d = new Date().getTime();
+			    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+		        var r = (d + Math.random()*16)%16 | 0;
+		        d = Math.floor(d/16);
+		        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+			    });
+			    return uuid;
+				}
+
+				var id = getUuid(); // ++this._uniqueIDIndex;
 				if (this._changes[id]) {
 					// Dupe.. create another..
 					id = this.getNewChangeId();
